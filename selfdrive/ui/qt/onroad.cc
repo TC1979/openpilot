d23134a5759f9f5e4186109fbe5abdde21dd5cc0
CT921 <tim.chen242@gmail.com>
2023-09-28 14:10:13 +0800

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1225,10 +1225,10 @@ void AnnotatedCameraWidget::drawTimSignals(QPainter &p) {
   constexpr int signalWidth = 142;
 
   // Calculate the vertical position for the turn signals
-  const int baseYPosition = 300;
+  const int baseYPosition = 350;
   // Calculate the x-coordinates for the turn signals
-  int leftSignalXPosition = width() / 2 - signalWidth * (blindSpotLeft ? 0 : 1);
-  int rightSignalXPosition = width() / 2 + signalWidth * (blindSpotRight ? 0 : 1);
+  int leftSignalXPosition = width() / 2 - signalWidth * (blindSpotLeft ? 2 : 0);
+  int rightSignalXPosition = width() / 2 + signalWidth * (blindSpotRight ? 2 : 0);
 
   // Enable Antialiasing
   p.setRenderHint(QPainter::Antialiasing);
