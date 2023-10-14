6c9981524e29a7cb8ac6d903fdb9d2bfa8518efc
CT921 <tim.chen242@gmail.com>
2023-10-14 16:16:54 +0800

    update

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -20,8 +20,8 @@ from openpilot.selfdrive.controls.vtsc import vtsc
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
-A_CRUISE_MIN_VALS_TOYOTA = [-0.53, -0.53, -0.53, -0.60, -0.60, -0.55,  -0.40]
-A_CRUISE_MIN_BP_TOYOTA =   [0.,    3.,    8.3,   14,    20.,   30.,   55.]
+A_CRUISE_MIN_VALS_TOYOTA = [-0.5, -0.5, -0.2, -0.3, -0.4, -0.5, -0.64, -0.52, -0.4,  -0.35]
+A_CRUISE_MIN_BP_TOYOTA =   [0.,    0.3,  0.35, 3.,   6.,   8.3,  14,    20.,  30.,   55.]
 A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 A_CRUISE_MAX_VALS_TOYOTA = [2.2, 2.0, 1.6, 1.12, 0.77, 0.70, 0.58, 0.4,  0.31, 0.11]  # Sets the limits of the planner accel, PID may exceed
