28ad41a57d5c593964d51d444e4477f2a1a9fc3e
CT921 <tim.chen242@gmail.com>
2023-11-23 14:44:17 +0800

    update

--- selfdrive/ui/qt/maps/map_panel.h
+++ selfdrive/ui/qt/maps/map_panel.h
@@ -9,7 +9,6 @@ class MapPanel : public QFrame {
 
 public:
   explicit MapPanel(const QMapboxGLSettings &settings, QWidget *parent = nullptr);
-  void setVisible(bool visible);
 
 signals:
   void mapPanelRequested();
