f512f9dbb4f5f0778fbe0e644fb9561b412d3702
CT921 <tim.chen242@gmail.com>
2024-02-05 02:40:24 +0800

    Update modeld.py

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -241,8 +241,9 @@ def main(demo=False):
       vec_desire[desire] = 1
 
     # Enable/disable nav features
-    timestamp_llk = sm["navModel"].locationMonoTime
-    nav_valid = sm.valid["navModel"] # and (nanos_since_boot() - timestamp_llk < 1e9)
+    # timestamp_llk = sm["navModel"].locationMonoTime
+    # nav_valid = sm.valid["navModel"] # and (nanos_since_boot() - timestamp_llk < 1e9)
+    # nav_enabled = nav_valid and params.get_bool("ExperimentalMode")
     nav_enabled = True
 
     if not nav_enabled:
