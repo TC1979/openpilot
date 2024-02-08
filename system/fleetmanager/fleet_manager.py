63d9728e282f089ac4fcd5ee38601d057f28377f
CT921 <tim.chen242@gmail.com>
2024-02-08 20:36:56 +0800

    update

--- system/fleetmanager/fleet_manager.py
+++ system/fleetmanager/fleet_manager.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import secrets
-from flask import Flask, render_template, Response, request, send_from_directory
+from flask import Flask, render_template, Response, request
 from openpilot.common.realtime import set_core_affinity
 import openpilot.system.fleetmanager.helpers as fleet
 from openpilot.system.hardware.hw import Paths
@@ -60,31 +60,6 @@ def route(route):
 @app.route("/footage")
 def footage():
   return render_template("footage.html", rows=fleet.all_routes())
-
-
-@app.route("/screenrecords/")
-@app.route("/screenrecords")
-def screenrecords():
-  rows = fleet.list_files(fleet.SCREENRECORD_PATH)
-  if not rows:
-    return render_template("error.html", error="no screenrecords found at:<br><br>" + fleet.SCREENRECORD_PATH)
-  return render_template("screenrecords.html", rows=rows, clip=rows[0])
-
-
-@app.route("/screenrecords/<clip>")
-def screenrecord(clip):
-  return render_template("screenrecords.html", rows=fleet.list_files(fleet.SCREENRECORD_PATH), clip=clip)
-
-
-@app.route("/screenrecords/play/pipe/<file>")
-def videoscreenrecord(file):
-  file_name = fleet.SCREENRECORD_PATH + file
-  return Response(fleet.ffplay_mp4_wrap_process_builder(file_name).stdout.read(), status=200, mimetype='video/mp4')
-
-
-@app.route("/screenrecords/download/<clip>")
-def download_file(clip):
-  return send_from_directory(fleet.SCREENRECORD_PATH, clip, as_attachment=True)
 
 
 @app.route("/about")
