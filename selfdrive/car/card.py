f5d374cb0f34c5edd6045327c28a0ce8746c3cd9
CT921 <tim.chen242@gmail.com>
2024-03-07 13:55:40 +0800

    update

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -54,6 +54,18 @@ class CarD:
     if not disengage_on_accelerator:
       self.CP.alternativeExperience |= ALTERNATIVE_EXPERIENCE.DISABLE_DISENGAGE_ON_GAS
 
+    dp_atl = self.params.get_bool("dp_atl")
+    if dp_atl:
+      self.CP.alternativeExperience |= ALTERNATIVE_EXPERIENCE.ALKA
+
+    auto_brakehold = self.params.get_bool("AleSato_AutomaticBrakeHold")
+    if auto_brakehold:
+      self.CP.alternativeExperience |= ALTERNATIVE_EXPERIENCE.ALLOW_AEB
+
+    sport_mode = self.params.get_bool("Marc_Dynamic_Follow") and self.CP.carFingerprint in TSS2_CAR
+    if sport_mode:
+      self.CP.alternativeExperience |= ALTERNATIVE_EXPERIENCE.RAISE_LONGITUDINAL_LIMITS_TO_ISO_MAX
+
     car_recognized = self.CP.carName != 'mock'
     openpilot_enabled_toggle = self.params.get_bool("OpenpilotEnabledToggle")
 
