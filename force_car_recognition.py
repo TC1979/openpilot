188e54cae5b9e47cde6925fe1f9afcd535920950
CT921 <tim.chen242@gmail.com>
2024-03-22 23:01:22 +0800

    update

--- force_car_recognition.py
+++ force_car_recognition.py
@@ -1,4 +1,5 @@
-filenames = ['/data/openpilot/selfdrive/car/top_tmp/HondaCars', '/data/openpilot/selfdrive/car/top_tmp/ToyotaCars', '/data/openpilot/selfdrive/car/top_tmp/VolkswagenCars']
+filenames = ['/data/openpilot/selfdrive/car/top_tmp/HondaCars', '/data/openpilot/selfdrive/car/top_tmp/HyundaiCars', '/data/openpilot/selfdrive/car/top_tmp/SubaruCars',
+             '/data/openpilot/selfdrive/car/top_tmp/ToyotaCars', '/data/openpilot/selfdrive/car/top_tmp/VolkswagenCars']
 
 with open('/data/openpilot/selfdrive/car/top_tmp/Cars', 'w') as outfile:
   for names in filenames:
