c1b10c40e7cc1e6689217fa7927ed983b9b7051d
CT921 <tim.chen242@gmail.com>
2024-03-22 16:15:06 +0800

    Update carstate.py

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -106,7 +106,7 @@ class CarState(CarStateBase):
     )
     ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    if self.CP.carFingerprint == CAR.PRIUS_V:
+    if self.CP.carFingerprint == CAR.TOYOTA_PRIUS_V:
       ret.vEgoCluster = ret.vEgo * 1.08987654321
     else:
       ret.vEgoCluster = ret.vEgo * 1.015  # minimum of all the cars
