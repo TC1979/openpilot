0775d13719c5395a557ecc850dbf98ab6a9aa9b1
CT921 <tim.chen242@gmail.com>
2024-03-23 00:05:43 +0800

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -794,8 +794,9 @@ if __name__ == "__main__":
 
 def main():
   for member, value in vars(CAR).items():
-    if not member.startswith("_") and not member.startswith("Flag") and not hasattr(value, 'flags'):
-      print(value)
+    if not member.startswith("_"):
+      if not hasattr(value, 'flags'):
+        print(value)
 
 if __name__ == "__main__":
   main()
