7ee6c5223f7eeadde5e00597a800e14f7c91d547
CT921 <tim.chen242@gmail.com>
2024-03-23 00:39:47 +0800

    Update values.py

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -280,7 +280,7 @@ if __name__ == "__main__":
 def main():
   for member, value in vars(CAR).items():
     if not member.startswith("_"):
-      if not hasattr(value, 'flags') or 'SubaruFlags' not in str(value.flags):
+      if not hasattr(value, 'flags') or 'GLOBAL_GEN2' not in str(value.flags):
         print(value)
 
 if __name__ == "__main__":
