3bfb1a024b3ec176387b89e7669669e700f944cf
CT921 <tim.chen242@gmail.com>
2024-04-02 15:58:23 +0800

    Update

--- release/build_release1.sh
+++ release/build_release1.sh
@@ -72,7 +72,9 @@ find . -name '*.o' -delete
 find . -name '*.os' -delete
 find . -name '*.pyc' -delete
 find . -name 'moc_*' -delete
+find . -name '*.cc' -delete
 find . -name '__pycache__' -delete
+find selfdrive/ui/ -name '*.h' -delete
 rm -rf .sconsign.dblite Jenkinsfile release/
 rm selfdrive/modeld/models/supercombo.onnx
 
