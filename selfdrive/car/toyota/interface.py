b169d6b137f462a73ad394111c7da4c4c683255a
CT921 <tim.chen242@gmail.com>
2024-09-12 18:25:12 +0800

    UPDATE

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -172,8 +172,8 @@ class CarInterface(CarInterfaceBase):
         ret.stoppingDecelRate = 0.1  # reach stopping target smoothly
 
     else:
-      tune.kiBP = [0.,   3.,    8.,    12.,   20.,  27.,  36.,  50]
-      tune.kiV = [0.322, 0.244, 0.224, 0.202, 0.17, 0.12, 0.08, 0.06]
+      tune.kiBP = [0.,   5.,   12.,   20.,  27.,  40.]
+      tune.kiV = [.35,   .2286,  .2086,  .1716, .10, .06]
 
       if candidate in TSS2_CAR:
         ret.vEgoStopping = 0.25
