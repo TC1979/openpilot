fbf28e16df19bc04b8125e76bb99b75ee6cd90aa
CT921 <tim.chen242@gmail.com>
2024-10-19 10:40:22 +0800

    Update mapd.py

--- selfdrive/mapd.py
+++ selfdrive/mapd.py
@@ -2,13 +2,24 @@
 import os
 import subprocess
 import urllib.request
-from openpilot.common.realtime import Ratekeeper
 import stat
+import json
+from openpilot.common.params import Params
+from openpilot.common.realtime import Ratekeeper
 
 VERSION = 'v1.9.0'
 URL = f"https://github.com/pfeiferj/openpilot-mapd/releases/download/{VERSION}/mapd"
 MAPD_PATH = '/data/media/0/osm/mapd'
 VERSION_PATH = '/data/media/0/osm/mapd_version'
+
+def get_gps_data(params):
+  gps_data = params.get("LastGPSPosition")
+  if gps_data:
+    try:
+      return json.loads(gps_data)
+    except json.JSONDecodeError:
+      print("Error decoding GPS data")
+  return None
 
 def download():
   mapd_dir = os.path.dirname(MAPD_PATH)
@@ -26,25 +37,26 @@ def download():
 
 def mapd_thread(sm=None, pm=None):
   rk = Ratekeeper(0.05, print_delay_threshold=None)
+  mem_params = Params("/dev/shm/params")
 
   while True:
     try:
-      if not os.path.exists(MAPD_PATH):
+      if not os.path.exists(MAPD_PATH) or not os.path.exists(VERSION_PATH):
         download()
         continue
-      if not os.path.exists(VERSION_PATH):
-        download()
-        continue
+
       with open(VERSION_PATH) as f:
         content = f.read()
         if content != VERSION:
           download()
           continue
 
+      gps_info = get_gps_data(mem_params)
+
       process = subprocess.Popen(MAPD_PATH)
       process.wait()
     except Exception as e:
-      print(e)
+      print(f"Error in mapd_thread: {e}")
 
     rk.keep_time()
 
