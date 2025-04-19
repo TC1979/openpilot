cfea5a0391bcd77b4c755e190d24bf69a53d45bf
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-04-19 13:36:38 +0800

    Revert "Rename experimental long"
    This reverts commit 512ac0ed7320972d94e8a6e4ee21b650cbbb84e0.

--- tools/lib/framereader.py
+++ tools/lib/framereader.py
@@ -105,7 +105,6 @@ def index_stream(fn, ft):
   frame_types, dat_len, prefix = hevc_index(fn)
   index = np.array(frame_types + [(0xFFFFFFFF, dat_len)], dtype=np.uint32)
   probe = ffprobe(fn, "hevc")
-  print('frame_types', frame_types, 'dat_len', dat_len, 'prefix', prefix)
 
   return {
     'index': index,
@@ -359,12 +358,10 @@ class StreamGOPReader(GOPReader):
     assert frame_type == FrameType.h265_stream
 
     self.fn = fn
-    # print('fn', fn)
 
     self.frame_type = frame_type
     self.frame_count = None
     self.w, self.h = None, None
-    print('frame_type', frame_type)
 
     self.prefix = None
     self.index = None
@@ -372,9 +369,6 @@ class StreamGOPReader(GOPReader):
     self.index = index_data['index']
     self.prefix = index_data['global_prefix']
     probe = index_data['probe']
-    print('index_data[\'index\']', self.index)
-    print('index_data[\'global_prefix\']', self.prefix)
-    print('probe', probe)
 
     self.prefix_frame_data = None
     self.num_prefix_frames = 0
@@ -388,11 +382,9 @@ class StreamGOPReader(GOPReader):
     assert self.first_iframe == 0
 
     self.frame_count = len(self.index) - 1
-    print('frame_count', self.frame_count)
 
     self.w = probe['streams'][0]['width']
     self.h = probe['streams'][0]['height']
-    print('w', self.w, 'h', self.h)
 
   def _lookup_gop(self, num):
     frame_b = num
@@ -427,9 +419,6 @@ class StreamGOPReader(GOPReader):
     skip_frames = 0
     if num < self.first_iframe:
       skip_frames = self.num_prefix_frames
-
-    print('frame_b', frame_b, 'num_frames', num_frames, 'skip_frames', skip_frames, 'rawdat', len(rawdat))
-    print()
 
     return frame_b, num_frames, skip_frames, rawdat
 
@@ -498,14 +487,10 @@ class GOPFrameReader(BaseFrameReader):
         return self.frame_cache[(num, pix_fmt)]
 
       frame_b, num_frames, skip_frames, rawdat = self.get_gop(num)
-      print('skip_frames', skip_frames)
-
 
       ret = decompress_video_data(rawdat, self.vid_fmt, self.w, self.h, pix_fmt)
-      print('ret.shape', ret.shape)
       ret = ret[skip_frames:]
-      print((ret.shape[0], num_frames))
-      assert ret.shape[0] == num_frames, (ret.shape[0], num_frames)
+      assert ret.shape[0] == num_frames
 
       for i in range(ret.shape[0]):
         self.frame_cache[(frame_b+i, pix_fmt)] = ret[i]
