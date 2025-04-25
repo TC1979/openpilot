f594a94211922cb1c9dc085dde28b78957516f25
CT921 <tim.chen242@gmail.com>
2025-04-25 15:52:43 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -61,8 +61,8 @@ COMFORT_BRAKE = 2.5
 # CRUISE_MIN_ACCEL = -1.2
 CRUISE_MAX_ACCEL = 1.6
 
-A_CRUISE_MIN_VALS = [-0.02, -0.01, -1.2]
-A_CRUISE_MIN_BP =   [ 0.,     .3,   40.]
+A_CRUISE_MIN_VALS = [-0.1, -0.2, -0.3, -0.5, -0.7, -0.9, -1.2]
+A_CRUISE_MIN_BP =   [ 0.,   .01,  .02,   .3,   1.,   3.,   6.]
 
 def get_cruise_min_accel(v_ego):
     return np.interp(v_ego, A_CRUISE_MIN_BP, A_CRUISE_MIN_VALS)
@@ -98,8 +98,8 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
     x_vel =  [0.,  40]
     y_dist = [1.0, 1.5]
   elif personality==log.LongitudinalPersonality.aggressive:
-    x_vel =  [0.,  40]
-    y_dist = [0.8, 1.1]
+    x_vel =  [0.,   40]
+    y_dist = [0.85, 1.1]
   else:
     raise NotImplementedError("Dynamic Follow personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
@@ -111,7 +111,7 @@ def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 4.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 4.0
+    return 3.5
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
