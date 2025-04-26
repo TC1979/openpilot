14ad67d8d6f821a6aac5fa7a73acd947c2219d88
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-04-26 09:22:58 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -61,8 +61,8 @@ COMFORT_BRAKE = 2.5
 # CRUISE_MIN_ACCEL = -1.2
 CRUISE_MAX_ACCEL = 1.6
 
-A_CRUISE_MIN_VALS = [-1.2, -0.2, -0.3, -0.6, -0.8, -1.0, -1.2]
-A_CRUISE_MIN_BP =   [ 0.,   .01,  .02,   .3,   6.,  20,   40]
+A_CRUISE_MIN_VALS = [-1.2, -0.6, -0.3, -0.6, -0.8, -1.2]
+A_CRUISE_MIN_BP =   [ 0.,   .01,  .02,   .3,   6.,  20]
 
 def get_cruise_min_accel(v_ego):
     return np.interp(v_ego, A_CRUISE_MIN_BP, A_CRUISE_MIN_VALS)
